63fc4b877b2eefbff6a0e5b0247ce58d3ec7468d
admin <admin@admin.com>
2018-01-24 12:58:52 +0900

    댓글카운트 수정

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):        
+        return self.comments.filter(approved_comment=True)    
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -31,6 +34,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text    
-
-    def approved_comments(self):        
-        return self.comments.filter(approved_comment=True)    
